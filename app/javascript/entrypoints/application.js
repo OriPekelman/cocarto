ed1dc19c5799731ba1f483cd5737ce47bb4598fa
Tristram Gräbener <tristramg@gmail.com>
2021-09-28 15:53:34 +0200

    Add hotwire-rails

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -14,6 +14,7 @@ console.log('Vite ⚡️ Rails')
 console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify.app/guide/rails')
 
 import "@hotwired/turbo-rails"
+import "./controllers"
 import * as ActiveStorage from "@rails/activestorage"
 
 ActiveStorage.start()
