61b9251ac52723b858d0c31fc16b0e72a03fff77
Tristram Gräbener <tristramg@gmail.com>
2021-09-29 18:53:58 +0200

    App a maplibre-gl map when editing a layer

--- app/javascript/controllers/application.ts
+++ app/javascript/controllers/application.ts
@@ -3,8 +3,8 @@ import { Application } from "@hotwired/stimulus"
 const application = Application.start()
 
 // Configure Stimulus development experience
-application.warnings = true
+// application.warnings = true
 application.debug    = false
-window.Stimulus      = application
+//window.Stimulus      = application
 
 export { application }
---
+++ app/javascript/controllers/map_controller.ts
@@ -0,0 +1,18 @@
+import { Controller } from '@hotwired/stimulus'
+import maplibregl from 'maplibre-gl'
+import map_t from 'maplibre-gl/types/ui/map'
+
+export default class extends Controller {
+  map: map_t
+
+  connect() {
+    this.map = new maplibregl.Map({
+      container: this.element.id,
+      style:
+        'https://api.maptiler.com/maps/basic/style.json?key=rF1iMNeNc3Eh3ES7Ke8H',
+      center: [0, 0],
+      zoom: 1,
+      accessToken: null,
+    })
+  }
+}
