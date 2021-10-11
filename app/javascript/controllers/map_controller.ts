6a0dc7a0a96378e27422720cc478495849191e7f
Tristram Gräbener <tristramg@gmail.com>
2021-10-11 14:15:00 +0200

    load maplibre-gl css

--- app/javascript/controllers/map_controller.ts
+++ app/javascript/controllers/map_controller.ts
@@ -1,9 +1,20 @@
 import { Controller } from '@hotwired/stimulus'
 import maplibregl from 'maplibre-gl'
-import map_t from 'maplibre-gl/types/ui/map'
+import 'maplibre-gl/dist/maplibre-gl.css'
+import Map from 'maplibre-gl/types/ui/map'
+import LngLat from 'maplibre-gl/types/geo/lng_lat'
+
+function popup(lngLat: LngLat) {
+  const markerHeight = 50, markerRadius = 10, linearOffset = 25;
+
+  return new maplibregl.Popup({offset: [0, -markerHeight], className: 'my-class', anchor: 'bottom'})
+  .setLngLat(lngLat)
+  .setHTML("<h1>Hello World!</h1>")
+  .setMaxWidth("300px");
+}
 
 export default class extends Controller {
-  map: map_t
+  map: Map
 
   connect() {
     this.map = new maplibregl.Map({
