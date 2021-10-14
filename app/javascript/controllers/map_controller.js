1d6eaec3c23f18a6c0a8b25d5d12b3d9a7b6ccce
Tristram Gräbener <tristramg@gmail.com>
2021-10-14 12:24:34 +0200

    Move away from typescript

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,40 +1,27 @@
 import { Controller } from '@hotwired/stimulus'
 import maplibregl from 'maplibre-gl'
 import 'maplibre-gl/dist/maplibre-gl.css'
-import Maplibre from 'maplibre-gl/types/ui/map'
-import LngLat from 'maplibre-gl/types/geo/lng_lat'
-import Popup from 'maplibre-gl/types/ui/popup'
-import Marker from 'maplibre-gl/types/ui/marker'
 
-function popup(lngLat: LngLat) : Popup {
-  const formTemplate = <HTMLTemplateElement>document.getElementById('points-form-template')
+function popup(lngLat) {
+  const formTemplate = document.getElementById('points-form-template')
   const clone = document.importNode(formTemplate.content, true)
 
-  return new maplibregl.Popup({anchor: 'bottom', })
+  return new maplibregl.Popup({anchor: 'bottom' })
   .setLngLat(lngLat)
   .setDOMContent(clone)
   .setMaxWidth("300px");
 }
 
-function marker(point: HTMLElement): Marker {
+function marker(point){
   const lng = +point.getAttribute('data-lng');
   const lat = +point.getAttribute('data-lat');
 
   return new maplibregl.Marker().setLngLat({lng, lat});
 }
 
-class MapBaseController extends Controller {
-  public longitudeDisplayTarget!: HTMLInputElement;
-  public longitudeFieldTarget!: HTMLInputElement;
-  public latitudeDisplayTarget!: HTMLInputElement;
-  public latitudeFieldTarget!: HTMLInputElement;
-  public mapTarget!: HTMLInputElement;
-}
 
-export default class extends (Controller  as typeof MapBaseController) {
+export default class extends Controller {
   static targets = [ "longitudeDisplay", "longitudeField", "latitudeDisplay", "latitudeField", "map", "point" ]
-  map: Maplibre
-  markers: Map<string, Marker>
 
   initialize() {
     this.markers = new Map();
@@ -60,7 +47,7 @@ export default class extends (Controller  as typeof MapBaseController) {
     });
   }
 
-  pointTargetConnected(point: HTMLElement) {
+  pointTargetConnected(point) {
     const id = point.getAttribute('id');
     const m = marker(point);
     this.markers.set(id, m);
@@ -69,7 +56,7 @@ export default class extends (Controller  as typeof MapBaseController) {
     }
   }
 
-  pointTargetDisconnected(point: HTMLElement) {
+  pointTargetDisconnected(point) {
     const id = point.getAttribute('id');
     const m = this.markers.get(id);
     m.remove();
