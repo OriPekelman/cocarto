317e77afbd4cc7ca091139c109e99239e182a393
Tristram Gräbener <tristramg@gmail.com>
2021-10-15 14:36:19 +0200

    Simplify a bit the views

--- app/models/field.rb
+++ app/models/field.rb
@@ -2,4 +2,7 @@ class Field < ApplicationRecord
   belongs_to :layer
   enum enum_field_type: {text: :text, float: :float, integer: :integer}
   validates :field_type, inclusion: {in: enum_field_types.keys}
+  def geometry_type
+    layer.geometry_type
+  end
 end
