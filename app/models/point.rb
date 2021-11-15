42220eba28a06005769e169ee85407d4f69b2bf5
Tristram Gräbener <tristramg@gmail.com>
2021-11-15 15:16:20 +0100

    The fields can be modified in the table and in sync

--- app/models/point.rb
+++ app/models/point.rb
@@ -2,4 +2,5 @@ class Point < ApplicationRecord
   belongs_to :layer
   after_create_commit -> { broadcast_append_to layer, target: "points-tbody" }
   after_destroy_commit -> { broadcast_remove_to layer }
+  after_update_commit -> { broadcast_replace_to layer }
 end
