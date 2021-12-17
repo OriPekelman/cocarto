04384707015ad78431c26cf87ab23995c0b39a35
Tristram Gräbener <tristramg@gmail.com>
2021-12-17 15:35:31 +0100

    Switch to importmap for javascript

---
+++ config/importmap.rb
@@ -0,0 +1,12 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application", preload: true
+pin "process", to: "https://ga.jspm.io/npm:@jspm/core@2.0.0-beta.13/nodelibs/browser/process-production.js"
+pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.0.1/dist/stimulus.js"
+pin "@hotwired/turbo-rails", to: "https://ga.jspm.io/npm:@hotwired/turbo-rails@7.1.0/app/javascript/turbo/index.js"
+pin "@hotwired/turbo", to: "https://ga.jspm.io/npm:@hotwired/turbo@7.1.0/dist/turbo.es2017-esm.js"
+pin "@rails/actioncable/src", to: "https://ga.jspm.io/npm:@rails/actioncable@6.1.4/src/index.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "maplibre-gl", to: "https://ga.jspm.io/npm:maplibre-gl@2.0.0-pre.6/dist/maplibre-gl.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
