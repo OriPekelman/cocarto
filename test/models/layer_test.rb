31d0ddb0b6bf38ee067c7c39a760455df801fb77
Tristram Gräbener <tristramg@gmail.com>
2022-03-29 17:49:53 +0200

    Fix fixtures and start testing

--- test/models/layer_test.rb
+++ test/models/layer_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class LayerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create a new layer" do
+    a = Layer.new geometry_type: :point
+    a.save!
+    assert a
+  end
+
+  test "a layer needs a geometry type" do
+    a = Layer.new
+    assert_raises(ActiveRecord::RecordInvalid) { a.save! }
+  end
+
+  test "a layer needs a valid geometry type" do
+    a = Layer.new geometry_type: :hypercube
+    assert_raises(ActiveRecord::RecordInvalid) { a.save! }
+  end
 end
