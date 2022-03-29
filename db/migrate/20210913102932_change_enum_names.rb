a9a6e4db4ca1daf923627c82feb31321e08d9293
Tristram Gräbener <tristramg@gmail.com>
2022-03-29 17:57:10 +0200

    Delete old migrations

--- db/migrate/20210913102932_change_enum_names.rb
+++
@@ -1,6 +0,0 @@
-class ChangeEnumNames < ActiveRecord::Migration[6.1]
-  def change
-    rename_enum :attribute_type, :attribute_type_enum
-    rename_enum :product_type, :geometry_type_enum
-  end
-end
--- db/migrate/20211013134347_add_field_type_to_field.rb
+++
@@ -1,5 +0,0 @@
-class AddFieldTypeToField < ActiveRecord::Migration[6.1]
-  def change
-    add_column :fields, :field_type, :fields_type_enum
-  end
-end
