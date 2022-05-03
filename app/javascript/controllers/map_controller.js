a2d440f87b58832d2ea3d2578d9338bf8a50155e
Tristram Gräbener <tristramg@gmail.com>
2022-05-03 10:09:03 +0200

    fix(map): add OSM attribution on the map
    closes #65

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -27,8 +27,13 @@ export default class extends Controller {
       style:
         'https://api.maptiler.com/maps/basic/style.json?key=rF1iMNeNc3Eh3ES7Ke8H',
       center: [0, 0],
-      zoom: 1
-    })
+      zoom: 1,
+      attributionControl: false,
+    }).addControl(new maplibre.AttributionControl({
+      customAttribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+      compact: false
+    }));
+
     this.trackers = new Trackers(this.map)
     this.markers.forEach(marker => marker.addTo(this.map))
 
