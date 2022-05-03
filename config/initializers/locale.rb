627c2a20eab98022f41ae3d332d2684a0ac0b462
Chrisahn75 <chris.ahona@gmail.com>
2022-05-03 17:14:37 +0200

    fix traduction en and fr

---
+++ config/initializers/locale.rb
@@ -0,0 +1,8 @@
+# Où la bibliothèque I18n doit rechercher les fichiers de traduction
+I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
+
+# Paramètres régionaux autorisés disponibles pour l'application
+I18n.available_locales = [:en, :fr]
+
+# Définir la locale par défaut sur autre chose que :en
+I18n.default_locale = :fr
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "layers#index"
 
-  resources :layers
-  resources :fields
-  resources :row_contents
-  get "/layers/:id/schema" => "layers#schema"
+  scope "(:locale)", locale: /en|fr/ do
+    resources :layers
+    resources :fields
+    resources :row_contents
+    get "/layers/:id/schema" => "layers#schema"
+    get '/:locale' => 'dashboard#index'
+  end
+
 end
