c19721ed73ec19d28cc517547a41736079541570
Chrisahn75 <chris.ahona@gmail.com>
2022-05-04 12:15:49 +0200

    make new test

--- test/system/layers_test.rb
+++ test/system/layers_test.rb
@@ -1,9 +1,12 @@
 require "application_system_test_case"
 
 class LayersTest < ApplicationSystemTestCase
-  test "visiting the index" do
+  test "visiting the index in french" do
     visit("fr/layers")
-
     assert_selector "h1", text: "Couche"
+  end
+    test "visiting the index in en" do
+      visit("en/layers")
+      assert_selector "h1", text:"Layers"
   end
 end
