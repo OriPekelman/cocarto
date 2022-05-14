d986c3b381286be535afbb97d1ae52b028dd97a5
Tristram Gräbener <tristramg@gmail.com>
2022-05-14 15:02:24 +0200

    feat(test) territories’ geojson and bounding boxes

--- test/models/territory_test.rb
+++ test/models/territory_test.rb
@@ -18,5 +18,17 @@ class TerritoryTest < ActiveSupport::TestCase
     GeojsonImporter.import("lib/assets/data_fixtures/regions.geojson", "Régions", "2022", true)
     regions = TerritoryCategory.find_by(name: "Régions")
     assert_equal 18, regions.territories.length
+
+    # Test that it calls the with_geojson scope
+    assert !regions.territories.first.geojson.nil?
+  end
+
+  test "we have geojson and geojson bounding" do
+    GeojsonImporter.import("lib/assets/data_fixtures/regions.geojson", "Régions", "2022", true)
+    guadeloupe = Territory.with_geojson.find_by(name: "Guadeloupe")
+    assert_in_epsilon guadeloupe.lng_min, -61.801
+    assert_in_epsilon guadeloupe.lat_min, 15.947
+    assert_in_epsilon guadeloupe.lng_max, -61.29
+    assert_in_epsilon guadeloupe.lat_max, 16.465
   end
 end
