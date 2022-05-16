e79a1597fba0fd06358612b35b6c046ccd0fa903
Tristram Gräbener <tristramg@gmail.com>
2022-05-16 17:22:00 +0200

    feat(controller): allow to filter territories by name

--- app/controllers/territory_categories_controller.rb
+++ app/controllers/territory_categories_controller.rb
@@ -1,9 +1,14 @@
 class TerritoryCategoriesController < ApplicationController
   def index
-    @categories = TerritoryCategory.all.limit
+    @categories = TerritoryCategory.all
   end
 
   def show
     @category = TerritoryCategory.find(params[:id])
+    @territories = if params[:q]
+      @category.territories.name_autocomplete(params[:q])
+    else
+      @category.territories
+    end
   end
 end
