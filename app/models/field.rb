3f979f08dd8270e73276e0c37396128ad3254cab
Tristram Gräbener <tristramg@gmail.com>
2022-05-17 12:10:01 +0200

    feat(model) add territory column type

--- app/models/field.rb
+++ app/models/field.rb
@@ -1,6 +1,6 @@
 class Field < ApplicationRecord
   belongs_to :layer
-  enum enum_field_type: {text: :text, float: :float, integer: :integer}
+  enum enum_field_type: {text: :text, float: :float, integer: :integer, territory: :territory}
   validates :field_type, inclusion: {in: enum_field_types.keys}
 
   after_create_commit -> do
