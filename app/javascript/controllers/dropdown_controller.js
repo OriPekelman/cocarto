fa3178fe9d38673f65951dbe269fa84345173958
Chrisahn75 <chris.ahona@gmail.com>
2022-05-24 15:36:58 +0200

    make a button traduction dropdown

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -2,7 +2,7 @@ import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
   toggle () {
-    console.log('toggle')
     this.element.classList.toggle('is-active')
   }
 }
+
