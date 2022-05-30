75f0fd2da9d0873f05f261b2ef171aefc06e2466
Tristram Gräbener <tristramg@gmail.com>
2022-05-30 16:36:20 +0200

    chore(user): test login/connexion/logout

---
+++ test/system/login_test.rb
@@ -0,0 +1,20 @@
+require "application_system_test_case"
+
+class TranslationsTest < ApplicationSystemTestCase
+  test "create an accout" do
+    visit("/?locale=en")
+    click_link "Sign up"
+    fill_in "user_email", with: "cabiai@amazonas.br"
+    fill_in "user_password", with: "canne à sucre"
+    fill_in "user_password_confirmation", with: "canne à sucre"
+    click_button "Sign up"
+    assert_selector "span", text: "Hello, cabiai@amazonas.br"
+
+    click_button "Sign out"
+    click_link "Log in"
+    fill_in "user_email", with: "cabiai@amazonas.br"
+    fill_in "user_password", with: "canne à sucre"
+    click_button "Log in"
+    assert_selector "span", text: "Hello, cabiai@amazonas.br"
+  end
+end
