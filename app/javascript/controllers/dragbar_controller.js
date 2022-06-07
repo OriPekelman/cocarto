df99342f92ad7a8826ea7a7600f1ac063ad5d554
Chrisahn75 <chris.ahona@gmail.com>
2022-06-07 16:58:59 +0200

    feat enable to make resizable map and table

---
+++ app/javascript/controllers/dragbar_controller.js
@@ -0,0 +1,29 @@
+import {Controller} from '@hotwired/stimulus'
+
+export default class extends Controller {
+    connect() {
+        console.log("isConnected");
+        console.log(this.leftTarget);
+        this.dragging = false;
+    }
+    static targets = ['left','right','dragbar']
+
+    mousedown (){
+    console.log("mousedown")
+    this.dragging=true;
+    }
+
+    mousemove (e){
+
+        if (this.dragging){    
+            console.log("mousemove-left");
+            this.leftTarget.style.width = `${e.clientX}px`;
+            this.rightTarget.style.width = `calc(100vw - ${e.clientX}px)`;
+        }
+    }
+    mouseup (){
+    console.log("mouseup");
+    this.dragging=false;    
+    }
+
+}
