a926bfe1192933083c207d13f360825584ab9ae6
Tristram Gräbener <tristramg@gmail.com>
2022-06-08 15:21:32 +0200

    chore(map controller) separate initialization in functions

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -27,50 +27,8 @@ export default class extends Controller {
   }
 
   connect () {
-
-    this.map = new_map(this.mapTarget)
-    const resizeObserver = new ResizeObserver (( ) => this.map.resize())
-    resizeObserver.observe(this.mapTarget)
-    this.trackers = new Trackers(this.map)
-    this.markers.forEach(marker => marker.addTo(this.map))
-
-    if (this.editableValue){
-      this.map.on('click', e => {
-        this.longitudeFieldTarget.value = e.lngLat.lng
-        this.latitudeFieldTarget.value = e.lngLat.lat
-        this.newPointFormTarget.requestSubmit()
-      })
-
-      this.map.on('mousemove', e => {
-        if ( Date.now() - this.lastMoveSent > 20) {
-          this.channel.mouse_moved(e.lngLat)
-          this.lastMoveSent = Date.now()
-        }
-      })
-    }
-
-    const _this = this
-    this.channel = consumer.subscriptions.create({channel: 'SharePositionChannel', layer: this.layerIdValue}, {
-      connected() {
-        console.log('connected')
-      },
-      disconnected() {
-        console.log('disconnected')
-      },
-      received(data) {
-        if(data.sessionId !== _this.sessionIdValue) {
-          _this.trackers.upsert(data)
-        }
-      },
-      mouse_moved(lngLat) {
-        return this.perform('mouse_moved', {
-          lngLat,
-          name: _this.usernameValue,
-          sessionId: _this.sessionIdValue,
-          layerId: _this.layerIdValue
-        });
-      },
-    });
+    this.#initMap()
+    this.#initActionCable()
   }
 
   pointTargetConnected (point) {
@@ -88,4 +46,53 @@ export default class extends Controller {
     m.remove()
     this.markers.delete(id)
   }
+
+  #initMap () {
+    this.map = new_map(this.mapTarget)
+    const resizeObserver = new ResizeObserver (( ) => this.map.resize())
+    resizeObserver.observe(this.mapTarget)
+    this.trackers = new Trackers(this.map)
+    this.markers.forEach(marker => marker.addTo(this.map))
+
+    if (this.editableValue){
+      this.map.on('click', e => {
+        this.longitudeFieldTarget.value = e.lngLat.lng
+        this.latitudeFieldTarget.value = e.lngLat.lat
+        this.newPointFormTarget.requestSubmit()
+      })
+
+      this.map.on('mousemove', e => {
+        if (Date.now() - this.lastMoveSent > 20) {
+          this.channel.mouse_moved(e.lngLat)
+          this.lastMoveSent = Date.now()
+        }
+      })
+    }
+  }
+
+  #initActionCable() {
+    const _this = this
+    this.channel = consumer.subscriptions.create({channel: 'SharePositionChannel', layer: this.layerIdValue}, {
+      connected() {
+        console.log('Cable SharePoisitonChannel connected')
+      },
+      disconnected() {
+        console.log('Cable SharePoisitonChannel disconnected')
+      },
+      received(data) {
+        if(data.sessionId !== _this.sessionIdValue) {
+          _this.trackers.upsert(data)
+        }
+      },
+      mouse_moved(lngLat) {
+        return this.perform('mouse_moved', {
+          lngLat,
+          name: _this.usernameValue,
+          sessionId: _this.sessionIdValue,
+          layerId: _this.layerIdValue
+        });
+      },
+    });
+  }
+
 }
