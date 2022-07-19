6054a46f87a90cfdae43404f04919eda362d2eec
Nicolas Bouilleaud <nico@bou.io>
2022-07-19 15:51:18 +0200

    Wrap devise and root in the (:locale) scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "main#index"
-
   scope "(:locale)", locale: Regexp.union(I18n.available_locales.map(&:to_s)) do
+    devise_for :users
+
+    root "main#index"
+
     resources :layers do
       member do
         get :schema
