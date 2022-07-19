4c711fa84ba867323871b1f298e092224c92f438
Nicolas Bouilleaud <nico@bou.io>
2022-07-19 12:37:12 +0200

    Fix strict loading default mode

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,10 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  after_initialize do
+    # We can’t set (yet?) the default strict loading *mode* in the app initializers.
+    # strict_loading on single objects leads to many issues with new records or with pundit
+    # See https://github.com/rails/rails/issues/41827#issuecomment-813282914 for some background
+    strict_loading!(self.class.strict_loading_by_default, mode: :n_plus_one_only)
+  end
 end
