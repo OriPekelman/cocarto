60618d90b204432619a771d4ce8b851376549066
Nicolas Bouilleaud <nico@bou.io>
2022-07-21 14:43:19 +0200

    Remove the confirmation_instructions and unlock_instructions templates
    we don’t use the confirmable and lockable devise modules

--- test/mailers/previews/devise_mailer_preview.rb
+++ test/mailers/previews/devise_mailer_preview.rb
@@ -1,14 +1,6 @@
 class DeviseMailerPreview < ActionMailer::Preview
-  def confirmation_instructions
-    Devise::Mailer.confirmation_instructions(User.first, "__token__")
-  end
-
   def reset_password_instructions
     Devise::Mailer.reset_password_instructions(User.first, "__token__")
-  end
-
-  def unlock_instructions
-    Devise::Mailer.unlock_instructions(User.first, "__token__")
   end
 
   def email_changed
