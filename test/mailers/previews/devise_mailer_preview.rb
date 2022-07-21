615cc6e9f26b12baa60e6ec2067c481d788740db
Nicolas Bouilleaud <nico@bou.io>
2022-07-21 11:49:55 +0200

    Remove the email_changed and password_changed templates
    send_email_changed_notification and send_password_change_notification are set to false

--- test/mailers/previews/devise_mailer_preview.rb
+++ test/mailers/previews/devise_mailer_preview.rb
@@ -2,12 +2,4 @@ class DeviseMailerPreview < ActionMailer::Preview
   def reset_password_instructions
     Devise::Mailer.reset_password_instructions(User.first, "__token__")
   end
-
-  def email_changed
-    Devise::Mailer.email_changed(User.first)
-  end
-
-  def password_change
-    Devise::Mailer.password_change(User.first)
-  end
 end
