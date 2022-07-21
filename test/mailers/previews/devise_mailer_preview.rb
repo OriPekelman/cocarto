37c032c501ffc8764ea97655300696109b2c06bb
Nicolas Bouilleaud <nico@bou.io>
2022-07-21 14:54:42 +0200

    Add mailer preview for invitation_instructions

--- test/mailers/previews/devise_mailer_preview.rb
+++ test/mailers/previews/devise_mailer_preview.rb
@@ -1,4 +1,8 @@
 class DeviseMailerPreview < ActionMailer::Preview
+  def invitation_instructions
+    Devise::Mailer.invitation_instructions(User.first, "__token__")
+  end
+
   def reset_password_instructions
     Devise::Mailer.reset_password_instructions(User.first, "__token__")
   end
