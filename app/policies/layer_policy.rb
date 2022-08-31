9ad5a5f53b882589090ef1e6d87a261ab3688049
Tristram Gräbener <tristramg@gmail.com>
2022-08-31 11:37:18 +0200

    Layer roles: editor and contributors can edit a layer

--- app/policies/layer_policy.rb
+++ app/policies/layer_policy.rb
@@ -1,19 +1,19 @@
 class LayerPolicy < ApplicationPolicy
-  def user_owns_record
-    return false if user.nil?
-
-    record.map.roles.owner.merge(user.roles).exists?
-  end
-
-  def show? = Role.exists?(map: record.map, user: user)
+  def show? = role.present?
 
   def schema? = show?
 
   def geojson? = show?
 
-  def edit? = user_owns_record
+  def edit? = update? || role&.contributor?
 
-  def update? = user_owns_record
+  def update? = role&.owner? || role&.editor?
 
-  def destroy? = user_owns_record
+  def destroy? = role&.owner?
+
+  private
+
+  def role
+    Role.find_by(map: record.map, user: user)
+  end
 end
