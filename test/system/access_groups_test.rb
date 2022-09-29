d33def41c5b9b29cf958f7885621e9e955653057
Tristram Gräbener <tristramg@gmail.com>
2022-09-29 17:14:39 +0200

    Extract login/logout test

--- test/system/access_groups_test.rb
+++ test/system/access_groups_test.rb
@@ -4,14 +4,9 @@ class AccessGroupsTest < ApplicationSystemTestCase
   test "the map owner can see the roles" do
     sign_in_as(users("reclus"), "refleurir")
     visit map_path(id: maps("boat"))
-    assert_text "Boating trip"
     click_link "Share…"
     assert_field "access_group_users_attributes_0_email", with: "elisee.reclus@commune.paris"
     assert_field "access_group_users_attributes_0_email", with: "cassini@carto.gouv.fr"
-
-    sign_out
-    visit map_path(id: maps("boat"))
-    assert_no_text "Boating trip"
   end
 
   test "a viewer can see the map, but not the roles" do
