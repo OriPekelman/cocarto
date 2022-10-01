8456a1b37f300f088723ab2b06520b0c44b1de77
Tristram Gräbener <tristramg@gmail.com>
2022-10-01 16:12:58 +0200

    Add a geocoder to find elements on a map (using nominatim)

--- app/javascript/lib/map_helpers.js
+++ app/javascript/lib/map_helpers.js
@@ -16,6 +16,47 @@ function newMap (node) {
   })).addControl(new maplibre.ScaleControl())
 }
 
+const geocoderApi = {
+  forwardGeocode: async (config) => {
+    const features = []
+    try {
+      const request =
+'https://nominatim.openstreetmap.org/search?q=' +
+config.query +
+'&format=geojson&polygon_geojson=1&addressdetails=1'
+      const response = await fetch(request)
+      const geojson = await response.json()
+      for (const feature of geojson.features) {
+        const center = [
+          feature.bbox[0] +
+(feature.bbox[2] - feature.bbox[0]) / 2,
+          feature.bbox[1] +
+(feature.bbox[3] - feature.bbox[1]) / 2
+        ]
+        const point = {
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: center
+          },
+          place_name: feature.properties.display_name,
+          properties: feature.properties,
+          text: feature.properties.display_name,
+          place_type: ['place'],
+          center
+        }
+        features.push(point)
+      }
+    } catch (e) {
+      console.error(`Failed to forwardGeocode with error: ${e}`)
+    }
+
+    return {
+      features
+    }
+  }
+}
+
 // Points, line strings and polygons are styled following
 // https://maplibre.org/maplibre-gl-js-docs/style-spec
 // This function returns the styles for the features (not the basemap)
@@ -129,4 +170,4 @@ function maplibreGLFeaturesStyle (color) {
   ]
 }
 
-export { newMap, maplibreGLFeaturesStyle }
+export { newMap, maplibreGLFeaturesStyle, geocoderApi }
