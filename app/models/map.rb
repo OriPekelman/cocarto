a2bb446759dbe52fbcbcb118db8fa344fbfb416e
Nicolas Bouilleaud <nico@bou.io>
2022-10-20 17:15:51 +0200

    Add query relations to get last map and row author

--- app/models/map.rb
+++ app/models/map.rb
@@ -15,4 +15,9 @@ class Map < ApplicationRecord
   # Through relations
   has_many :users, through: :access_groups, inverse_of: :maps
   has_many :rows, through: :layers, inverse_of: :map
+
+  # Query as relations
+  has_one :layer_with_last_updated_row, -> { joins(:rows).order("rows.updated_at": :desc) }, class_name: "Layer" # rubocop:disable Rails/InverseOf, Rails/HasManyOrHasOneDependent
+  has_one :last_updated_row, through: :layer_with_last_updated_row, source: :last_updated_row
+  has_one :last_updated_row_author, through: :last_updated_row, source: :author
 end
