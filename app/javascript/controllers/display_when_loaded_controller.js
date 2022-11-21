a0c35ee2de67fd205eb76a5a5789c6b916c8f2e0
Tristram Gräbener <tristramg@gmail.com>
2022-11-21 14:45:31 +0100

    Remove display-when-loaded controller
    This was an ugly hack to improve performances with large tables
    
    However, this is no longer needed: with all the layers shown on the same
    map, the tables are hidden by design

--- app/javascript/controllers/display_when_loaded_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from '@hotwired/stimulus'
-
-export default class extends Controller {
-  connect () {
-    // Loading time optimization based on
-    // https://patdavid.net/2019/02/displaying-a-big-html-table/
-    this.element.classList.remove('display-when-loaded')
-  }
-}
