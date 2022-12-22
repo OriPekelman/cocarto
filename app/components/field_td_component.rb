d40473090905daa6d7e85d72370c71b9c9b3e88f
Tristram Gräbener <tristramg@gmail.com>
2022-12-22 16:38:08 +0100

    FieldTdComponent: make field_tag a public function for add_single_row page

--- app/components/field_td_component.rb
+++ app/components/field_td_component.rb
@@ -8,6 +8,25 @@ class FieldTdComponent < ViewComponent::Base
     @author_id = author_id
   end
 
+  def field_tag
+    case @field.field_type
+    when "integer"
+      number_field_tag field_name, @value, opts.merge(class: "numerical", step: "1")
+    when "float"
+      number_field_tag field_name, @value, opts.merge(class: "numerical", step: "any")
+    when "territory"
+      render SearchComponent.new(field: field_name, territory: @value, form: opts[:form], field_id: @field.id, layer_id: nil)
+    when "date"
+      date_field_tag field_name, @value, opts
+    when "boolean"
+      check_box_tag field_name, "1", @value == "1", opts
+    when "enum"
+      select_tag field_name, options_for_select(@field.enum_values, @value), opts.merge(include_blank: true)
+    else
+      text_field_tag field_name, @value, opts.merge(class: "input")
+    end
+  end
+
   private
 
   def html_class
@@ -28,24 +47,5 @@ class FieldTdComponent < ViewComponent::Base
 
   def field_name
     "row[fields_values][#{@field.id}]"
-  end
-
-  def field_tag
-    case @field.field_type
-    when "integer"
-      number_field_tag field_name, @value, opts.merge(class: "numerical", step: "1")
-    when "float"
-      number_field_tag field_name, @value, opts.merge(class: "numerical", step: "any")
-    when "territory"
-      render SearchComponent.new(field: field_name, territory: @value, form: opts[:form], field_id: @field.id, layer_id: nil)
-    when "date"
-      date_field_tag field_name, @value, opts
-    when "boolean"
-      check_box_tag field_name, "1", @value == "1", opts
-    when "enum"
-      select_tag field_name, options_for_select(@field.enum_values, @value), opts.merge(include_blank: true)
-    else
-      text_field_tag field_name, @value, opts.merge(class: "input")
-    end
   end
 end
