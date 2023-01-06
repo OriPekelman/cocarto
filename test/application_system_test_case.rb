2581eae61e72b310dc7a9ab389a9b9ea4f4bc629
Nicolas Bouilleaud <nico@bou.io>
2023-01-06 19:02:29 +0100

    Experimental: use ssl in capybara tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,16 +3,29 @@ require "capybara/cuprite"
 require "ferrum_logger"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  if ENV["CI"] == "true"
-    # When using the browser in docker, we must pass the server’s url
-    Capybara.app_host = "http://#{`hostname`.strip&.downcase}".freeze
-    # and the server must bind from an other network
-    Capybara.server_host = "0.0.0.0"
-  end
-  # In the CI, the browser is a chrome service not launched by cuprite
-  ci_options = (ENV["CI"] == "true") ? {url: "http://browserless-chrome:3000"} : {}
+  Capybara.server_host = "0.0.0.0" # Allow connections from the local network
+  # SSL stuff, WIP
+  # Capybara.server_port = 3001 # this can be left unspecified
+  Capybara.server = :puma, {Host: "ssl://#{Capybara.server_host}"} # TODO Add Silent: true
+  # Specify the server hostname where the (remote) browser will connect
+  Capybara.app_host = "https://#{`hostname`.strip&.downcase}" # % Capybara.server_port
+  # Capybara.app_host = "https://#{"127.0.0.1".strip&.downcase}:%d" % Capybara.server_port # <- this works with allow-insecure-localhost
 
-  driven_by :cuprite, options: {logger: FerrumLogger.new, js_error: true}.merge(ci_options)
+  driver_options = {
+    logger: FerrumLogger.new, js_error: true,
+    browser_options: {
+      # "allow-insecure-localhost" => nil, # this works when connecting to 127.0.0.1
+      # "ignore-certificate-errors-spki-list" => nil # this needs something from the localhost gem
+      "ignore-certificate-errors" => nil # this is undocumented? but works locally (when connecting by hostname)
+    }
+  }
+
+  if ENV["CI"] == "true"
+    # In the CI, the browser is a chrome service not launched by cuprite
+  driver_options.merge!({url: "http://browserless-chrome:3000"})
+  end
+
+  driven_by :cuprite, options: driver_options
 
   def sign_in_as(user, password)
     visit user_session_path
