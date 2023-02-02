803c6964cf1a9d0ea6662b9a8e19fb80db7ef918
Tristram Gräbener <tristramg@gmail.com>
2023-02-02 15:32:32 +0100

    Test image download

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,12 +3,16 @@ require "capybara/cuprite"
 require "ferrum_logger"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  # Wait for at most DOWNLOAD_TIMEOUT seconds until all downloads are finished
+  DOWNLOAD_TIMEOUT = 10
   # Allow connections from the local network
   Capybara.server_host = "0.0.0.0"
   # Run server in https (using a self-signed certificate, automatically with the localhost gem)
   Capybara.server = :puma, {Host: "ssl://#{Capybara.server_host}", Silent: true}
   # Specify the server hostname (the local machine) where the (remote) browser will connect.
   Capybara.app_host = "https://#{`hostname`.strip&.downcase}"
+  # Where will capybara save screenshots, downloaded files…
+  Capybara.save_path = ENV.fetch("CAPYBARA_ARTIFACTS", "./tmp/capybara")
 
   driver_options = {logger: FerrumLogger.new, js_errors: true}
   # In the CI, the browser is a chrome service (not a process launched by cuprite)
@@ -46,5 +50,12 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   # Otherwise capybara will click on the button, and nothing happens
   def wait_until_dropdown_controller_ready
     assert page.has_css?('[data-dropdown-loaded-value="true"]')
+  end
+
+  def wait_all_downloads
+    downloads = Pathname.new(Capybara.save_path)
+    Timeout.timeout(DOWNLOAD_TIMEOUT) do
+      sleep 0.1 until downloads.glob("*.crdownload").blank?
+    end
   end
 end
