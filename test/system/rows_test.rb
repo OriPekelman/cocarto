46f03a69d828d12065faef261fa703393548b8aa
Tristram Gräbener <tristramg@gmail.com>
2023-02-08 12:23:11 +0100

    system test: wait more for the map to be loaded

--- test/system/rows_test.rb
+++ test/system/rows_test.rb
@@ -5,11 +5,13 @@ class RowsTest < ApplicationSystemTestCase
     sign_in_as(users("reclus"), "refleurir")
 
     visit map_path(id: maps("restaurants"))
+    # We make sure to wait that the map is loaded
+    map = find ".maplibregl-map[data-loaded]", wait: 10
     click_on "Display the table for this layer", match: :first
     click_on "Add a point"
 
     assert_changes -> { layers(:restaurants).rows.count("*") }, from: 1, to: 2 do
-      find(".maplibregl-map[data-loaded]").click
+      map.click
     end
   end
 end
