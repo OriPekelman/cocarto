1e7f2338d3e90da2f5a9b9c287d370f558bb0f9d
Tristram Gräbener <tristramg@gmail.com>
2023-02-08 13:08:14 +0100

    fix(261): send the correct header when layer is updated

--- test/system/rows_test.rb
+++ test/system/rows_test.rb
@@ -14,4 +14,22 @@ class RowsTest < ApplicationSystemTestCase
       map.click
     end
   end
+
+  test "add rows to the table" do
+    sign_in_as(users("reclus"), "refleurir")
+
+    visit map_path(id: maps("restaurants"))
+    map = find ".maplibregl-map[data-loaded]", wait: 10
+    click_on "Display the table for this layer", match: :first
+
+    assert_selector ".row", count: 1
+
+    click_on "Add a point"
+    map.click
+
+    click_on "Add a point"
+    map.click(x: 100, y: 100) # The offset is there to avoid clicking on the previously created marker
+
+    assert_selector ".row", count: 3
+  end
 end
