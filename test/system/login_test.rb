37ad7306c6d115f67411170175280e68fc531bd2
Nicolas Bouilleaud <nico@bou.io>
2023-02-13 15:12:20 +0100

    Fix system tests

--- test/system/login_test.rb
+++ test/system/login_test.rb
@@ -9,17 +9,17 @@ class LoginTest < ApplicationSystemTestCase
     fill_in "user_password_confirmation", with: "canne à sucre"
     click_button "Sign up"
 
-    assert_selector "span", text: "cabiai@amazonas.br"
+    assert_button "cabiai@amazonas.br"
 
     wait_until_dropdown_controller_ready
-    find("span", text: "cabiai@amazonas.br").click
+    click_button "cabiai@amazonas.br"
     click_button "Sign out"
     click_link "Log in"
     fill_in "user_email", with: "cabiai@amazonas.br"
     fill_in "user_password", with: "canne à sucre"
     click_button "Log in"
 
-    assert_selector "span", text: "cabiai@amazonas.br"
+    assert_button "cabiai@amazonas.br"
   end
 
   test "login and logout" do
