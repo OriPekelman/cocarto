993f7968599ffdca09c877c93cdefe59e2866cc5
Nicolas Bouilleaud <nico@bou.io>
2023-02-14 15:23:54 +0100

    Improve css class names for layer-table

--- app/components/row_component.rb
+++ app/components/row_component.rb
@@ -20,14 +20,14 @@ class RowComponent < ViewComponent::Base
     case @row.layer.geometry_type
     when "territory"
       tag.td(render(SearchComponent.new(form: form_id, field: "row[territory_id]", territory: @row.territory, field_id: nil, layer_id: @row.layer_id)), class: "table-field") +
-        tag.td(@row.territory.code, class: "layer-table__td__numerical")
+        tag.td(@row.territory.code, class: "layer-table__td--numerical")
     when "point"
-      tag.td(number_with_precision(@row.geo_lng_min, precision: 6), class: "layer-table__td__numerical") +
-        tag.td(number_with_precision(@row.geo_lat_min, precision: 6), class: "layer-table__td__numerical")
+      tag.td(number_with_precision(@row.geo_lng_min, precision: 6), class: "layer-table__td--numerical") +
+        tag.td(number_with_precision(@row.geo_lat_min, precision: 6), class: "layer-table__td--numerical")
     when "line_string"
-      tag.td(number_to_human(@row.geo_length, units: :length), class: "layer-table__td__numerical")
+      tag.td(number_to_human(@row.geo_length, units: :length), class: "layer-table__td--numerical")
     when "polygon"
-      tag.td(number_to_human(@row.geo_area, units: :area), class: "layer-table__td__numerical")
+      tag.td(number_to_human(@row.geo_area, units: :area), class: "layer-table__td--numerical")
     end
   end
 
