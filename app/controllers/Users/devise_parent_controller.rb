b623ee89f90d8489ca47e047690a53eaeab76b5e
Nicolas Bouilleaud <nico@bou.io>
2023-03-10 21:29:01 +0100

    Remove now unneeded workarounds for Devise and Turbo and follow guide

--- app/controllers/Users/devise_parent_controller.rb
+++ app/controllers/Users/devise_parent_controller.rb
@@ -1,23 +1,3 @@
 class Users::DeviseParentController < ApplicationController
   before_action :skip_authorization
-
-  # TODO: Temporary fix for Devise and Turbo. Will be fixed in Devise 4.9.0.
-  # See https://github.com/heartcombo/devise/pull/5548
-  class Responder < ActionController::Responder
-    def to_turbo_stream
-      controller.render(options.merge(formats: :html))
-    rescue ActionView::MissingTemplate => error
-      if get?
-        raise error
-      elsif has_errors? && default_action
-        render rendering_options.merge(formats: :html,
-          status: :unprocessable_entity)
-      else
-        redirect_to navigation_location
-      end
-    end
-  end
-
-  self.responder = Responder
-  respond_to :html, :turbo_stream
 end
