4c5d7f2b37ecdf734724249c0ebc7901628e578f
Nicolas Bouilleaud <nico@bou.io>
2023-03-14 16:31:50 +0100

    Add a system test for the rows/new form

--- test/system/rows_test.rb
+++ test/system/rows_test.rb
@@ -32,4 +32,22 @@ class RowsTest < ApplicationSystemTestCase
 
     assert_selector ".row", count: 3
   end
+
+  test "add a point from the rows/new form" do
+    sign_in_as(users("reclus"), "refleurir")
+
+    visit new_layer_row_path(layer_id: layers(:restaurants))
+
+    fill_in "Name", with: "Le Bastringue"
+    fill_in "Rating", with: "5"
+    fill_in "Table Size", with: "9"
+
+    # The point location is set via js after the page is loaded; wait for the geojson field value to be set. (Also, it’s invisible.)
+    # See views/rows/new.html.erb and map_add_point_controller.js
+    find("#row_geojson[value*='{\"type\":\"Point\",\"coordinates\":']", visible: false)
+
+    assert_changes -> { layers(:restaurants).rows.count("*") }, from: 1, to: 2 do
+      click_on "Add this point"
+    end
+  end
 end
