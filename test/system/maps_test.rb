6f2cfafb0408c2ea0f4f0a212b8de8f18a30e76b
Nicolas Bouilleaud <nico@bou.io>
2023-04-14 10:22:24 +0200

    Make MapsTest#download_as_an_image more reliable

--- test/system/maps_test.rb
+++ test/system/maps_test.rb
@@ -37,6 +37,7 @@ class MapsTest < ApplicationSystemTestCase
     sign_in_as(users("reclus"), "refleurir")
 
     visit map_path(id: maps("restaurants"))
+    find_map_loaded
     click_link "Export as image", href: nil
     wait_all_downloads
 
