80f9b2c6b3345811a3136ea4dd2600c6b7f6297b
Nicolas Bouilleaud <nico@bou.io>
2023-05-17 18:24:28 +0200

    Simplify geojson import

--- app/services/import_export/geojson_importer.rb
+++ app/services/import_export/geojson_importer.rb
@@ -3,17 +3,7 @@ module ImportExport
     def import_rows
       geojson = RGeo::GeoJSON.decode(@input, geo_factory: RGeo::Cartesian.factory(srid: 4326))
       geojson.each do |feature|
-        values = feature.properties
-        geometry = if feature.geometry.geometry_type.type_name.start_with?("Multi")
-          if feature.geometry.size > 1
-            Rails.logger.debug "Feature has more than one element, skipping the others"
-          end
-          feature.geometry[0]
-        else
-          feature.geometry
-        end
-
-        import_row(geometry, values)
+        import_row(feature.geometry, feature.properties)
       end
     end
   end
