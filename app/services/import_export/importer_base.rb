0fb89b75af8777a50427dae12b3e0bcecc7a7d15
Nicolas Bouilleaud <nico@bou.io>
2023-05-22 17:42:11 +0200

    Store errors and warnings during import

--- app/services/import_export/importer_base.rb
+++ app/services/import_export/importer_base.rb
@@ -1,6 +1,29 @@
 require "csv"
 
 module ImportExport
+  class ImportGlobalError < StandardError; end
+
+  ## Result of the whole import operation
+  # contains both a global status and results for each entity that was (or wasn't) imported.
+  class ImportResult
+    attr_accessor :global_error
+    attr_reader :entity_results
+
+    EntityResult = Struct.new(:did_save, :parsing_error, :validation_errors, :validation_warnings)
+
+    def initialize
+      @entity_results = []
+    end
+
+    def entity_result(index)
+      @entity_results[index] ||= EntityResult.new
+    end
+
+    def success?
+      global_error.nil? && @entity_results.all? { |entity_result| entity_result.did_save }
+    end
+  end
+
   class ImporterBase
     # @options:
     # - author
@@ -15,14 +38,27 @@ module ImportExport
     end
 
     def import
-      ApplicationRecord.transaction { import_rows }
+      @result = ImportResult.new
+      ApplicationRecord.transaction do
+        begin
+          import_rows
+        rescue ImportGlobalError => e
+          @result.global_error = e.cause
+        end
+
+        raise ActiveRecord::Rollback unless @result.success?
+      end
+
+      @result
     end
 
     # implemented by subclasses
     def import_rows = raise NotImplementedError
 
     # import a single row
-    def import_row(geometry, values)
+    # The index is used as context for the concrete subclass.
+    # (the line number for CSV, the feature index for geojson…)
+    def import_row(geometry, values, index)
       # map keys
       values = values.transform_keys do |k|
         @mapping[k] || k
@@ -42,7 +78,18 @@ module ImportExport
       end
       row.fields_values = values
 
-      row.save!
+      did_save = row.save
+
+      set_entity_result(index, did_save: did_save, validation_errors: row.errors.presence, validation_warnings: row.warnings.presence)
+    end
+
+    # save the result for an index; this is called by import_row, but also by subclasses if needed
+    def set_entity_result(index, did_save: nil, parsing_error: nil, validation_errors: nil, validation_warnings: nil)
+      entity_result = @result.entity_result(index)
+      entity_result.did_save = did_save
+      entity_result.parsing_error = parsing_error
+      entity_result.validation_errors = validation_errors
+      entity_result.validation_warnings = validation_warnings
     end
   end
 end
