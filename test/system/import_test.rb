3c58451ff31ac9ee5affca5cd4e8fec214244e79
Nicolas Bouilleaud <nico@bou.io>
2023-06-20 18:11:05 +0200

    Add Import Result system tests

--- test/system/import_test.rb
+++ test/system/import_test.rb
@@ -17,7 +17,26 @@ class ImportTest < ApplicationSystemTestCase
     select "Name", from: "ID column"
     click_on "Import"
 
+    assert_text "Import successful"
+    assert_text "Imported 2 rows"
+
+    click_on "OK"
+
     assert_selector "input[value='L’Antipode']", count: 1
     assert_selector "input[value='Le Bastringue']", count: 1
+  end
+
+  test "import failure" do  # rubocop:disable Minitest/MultipleAssertions
+    sign_in_as(users("reclus"), "refleurir")
+    visit layer_path(id: layers("restaurants"))
+    wait_until_turbo_stream_connected
+
+    # Import restaurants (with the Name as the key)
+    click_on "Import…"
+    attach_file("file", file_fixture("touladi.png").to_path, make_visible: true)
+    click_on "Import"
+
+    assert_text "Import failed"
+    assert_text "Invalid byte sequence in UTF-8 in line 1."
   end
 end
