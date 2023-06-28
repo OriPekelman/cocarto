14367e2bd2bd5cbd733f528d86f27f77da6bc7e1
Nicolas Bouilleaud <nico@bou.io>
2023-06-28 12:54:19 +0200

    Add system test for WFS import

--- test/system/import_test.rb
+++ test/system/import_test.rb
@@ -1,42 +1,68 @@
 require "application_system_test_case"
+require "fixtures/mock_wks_server"
 
 class ImportTest < ApplicationSystemTestCase
-  test "import stuff" do  # rubocop:disable Minitest/MultipleAssertions
-    sign_in_as(users("reclus"), "refleurir")
-    visit layer_path(id: layers("restaurants"))
-    wait_until_turbo_stream_connected
+  class FileImportTest < ImportTest
+    test "import from a csv file" do  # rubocop:disable Minitest/MultipleAssertions
+      sign_in_as(users("reclus"), "refleurir")
+      visit layer_path(id: layers("restaurants"))
+      wait_until_turbo_stream_connected
 
-    # Check that row is not present yet
-    assert_selector "input[value='L’Antipode']", count: 1
-    refute_selector "input[value='Le Bastringue']"
+      # Check that row is not present yet
+      assert_selector "input[value='L’Antipode']", count: 1
+      refute_selector "input[value='Le Bastringue']"
 
-    # Reimport restaurants (with the Name as the key)
-    click_on "Import…"
-    attach_file("file", file_fixture("restaurants.csv").to_path, make_visible: true)
-    find(".form__details-summary").click
-    select "Name", from: "ID column"
-    click_on "Import"
+      # Reimport restaurants (with the Name as the key)
+      click_on "Import…"
+      attach_file("file", file_fixture("restaurants.csv").to_path, make_visible: true)
+      find(".form__details-summary").click
+      select "Name", from: "ID column"
+      click_on "Import"
 
-    assert_text "Import successful"
-    assert_text "Imported 2 rows"
+      assert_text "Import successful"
+      assert_text "Imported 2 rows"
 
-    click_on "OK"
+      click_on "OK"
 
-    assert_selector "input[value='L’Antipode']", count: 1
-    assert_selector "input[value='Le Bastringue']", count: 1
+      assert_selector "input[value='L’Antipode']", count: 1
+      assert_selector "input[value='Le Bastringue']", count: 1
+    end
+
+    test "import failure" do  # rubocop:disable Minitest/MultipleAssertions
+      sign_in_as(users("reclus"), "refleurir")
+      visit layer_path(id: layers("restaurants"))
+      wait_until_turbo_stream_connected
+
+      # Import restaurants (with the Name as the key)
+      click_on "Import…"
+      attach_file("file", file_fixture("touladi.png").to_path, make_visible: true)
+      click_on "Import"
+
+      assert_text "Import failed"
+      assert_text "Invalid byte sequence in UTF-8 in line 1."
+    end
   end
 
-  test "import failure" do  # rubocop:disable Minitest/MultipleAssertions
-    sign_in_as(users("reclus"), "refleurir")
-    visit layer_path(id: layers("restaurants"))
-    wait_until_turbo_stream_connected
+  class ServerImportTest < ImportTest
+    include MockWfsServer
 
-    # Import restaurants (with the Name as the key)
-    click_on "Import…"
-    attach_file("file", file_fixture("touladi.png").to_path, make_visible: true)
-    click_on "Import"
+    test "import from a wfs server" do  # rubocop:disable Minitest/MultipleAssertions
+      sign_in_as(users("cassini"), "générations12345")
+      visit layer_path(id: layers("hiking_paths"))
+      wait_until_turbo_stream_connected
 
-    assert_text "Import failed"
-    assert_text "Invalid byte sequence in UTF-8 in line 1."
+      # Reimport restaurants (with the Name as the key)
+      click_on "Import…"
+      fill_in "url", with: "http://#{`hostname`.strip&.downcase}:9090"
+      fill_in "input_layer_name", with: "TEST_FEATURE_NAME"
+      click_on "Import"
+
+      assert_text "Import successful"
+      assert_text "Imported 4 rows"
+
+      click_on "OK"
+
+      assert_selector "input[value='Tracé numéro un']", count: 1
+    end
   end
 end
