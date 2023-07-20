6708baa2dbe56ce731a8535a462b75a9d2051551
Nicolas Bouilleaud <nico@bou.io>
2023-07-20 11:37:48 +0200

    Cleanup i18n for userrole types

--- app/helpers/map_permissions_helper.rb
+++ app/helpers/map_permissions_helper.rb
@@ -1,8 +1,7 @@
 module MapPermissionsHelper
   def options_for_role_type_select(record)
-    UserRole.human_attribute_name("role_types.owner")
     options = record.class.role_types.map do |role_type|
-      [record.class.human_attribute_name("role_types.#{role_type[0]}"), role_type[1]]
+      [UserRole.human_attribute_name("role_types.#{role_type[0]}"), role_type[1]]
     end
     options_for_select(options, record.role_type)
   end
