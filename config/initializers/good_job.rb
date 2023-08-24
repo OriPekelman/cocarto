6f079cadb040aa5267533f55c7c1f5a278a32176
Nicolas Bouilleaud <nico@bou.io>
2023-08-24 09:59:32 +0200

    Enforce strict-loading only for our ApplicationRecord subclasses
    This is due to GoodJob’s views having some N+1 🙈

--- config/initializers/good_job.rb
+++ config/initializers/good_job.rb
@@ -1,7 +1,3 @@
 Rails.application.config.after_initialize do
-  GoodJob.active_record_parent_class = "ApplicationRecord"
-
   GoodJob.on_thread_error = ->(exception) { Rails.error.report(exception, handled: false) } # Sentry registers as a standard ActiveSupport::ErrorReporter
-
-  GoodJob::Job.strict_loading_by_default = false # The GoodJob::Jobs#show page in the dashboard has N+1 queries ¯\_(ツ)_/¯
 end
