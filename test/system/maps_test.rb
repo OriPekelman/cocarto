9a717f6cf47029a163bbe56d3082dbd2b155df30
Tristram Gräbener <tristramg@gmail.com>
2023-09-06 17:41:40 +0200

    By default open the first layer

--- test/system/maps_test.rb
+++ test/system/maps_test.rb
@@ -26,6 +26,9 @@ class MapsTest < ApplicationSystemTestCase
 
     assert_selector "h2", text: "Restaurants"
 
+    # The layer is open and we can see its content
+    assert_text "L’Antipode"
+
     # reclus has no access to hiking
     visit map_path(id: maps("hiking"))
 
