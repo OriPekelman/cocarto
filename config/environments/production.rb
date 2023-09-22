8cd2de7ec3616a4036ffc13c43ce0da09b9ba53d
Ori Pekelman <ori@pekelman.com>
2023-09-22 12:47:57 +0200

    no REDIS_URL in build

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,7 @@ Rails.application.configure do
 
   # Prepend all log lines with the following tags.
   config.log_tags = [:request_id]
-
+  if ENV["REDIS_URL"].present?
   # Use a different cache store in production.
   config.cache_store = :redis_cache_store, {
     url: ENV.fetch("REDIS_URL"),
@@ -66,6 +66,7 @@ Rails.application.configure do
     pool_size: 5,
     pool_timeout: 5
   }
+  end
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
