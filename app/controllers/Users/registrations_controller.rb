85d7832fca1aedf7f00abedfc1dd08adc36179b0
enkhe-D <enkhedeny07@gmail.com>
2023-09-27 15:49:04 +0200

    add field in registration controller

--- app/controllers/Users/registrations_controller.rb
+++ app/controllers/Users/registrations_controller.rb
@@ -4,6 +4,8 @@ class Users::RegistrationsController < DeviseInvitable::RegistrationsController
   # Allow sign up (registrations#new and registrations#create) if the current_user is actually anonymous
   skip_before_action :require_no_authentication, if: -> { warden.authenticated? && current_user&.anonymous? }
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def create
     if current_user&.anonymous?
       anonymous_user = current_user
@@ -20,5 +22,12 @@ class Users::RegistrationsController < DeviseInvitable::RegistrationsController
     else
       redirect_back(fallback_location: :edit_user_registration_path, alert: t("common.failed", msg: resource.errors.full_messages.to_sentence))
     end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:display_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:display_name])
   end
 end
